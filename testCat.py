c44870ad15e9e09df0a990c033e547697635d27c
volkovda98 <81383854+volkovda98@users.noreply.github.com>
2021-03-31 00:12:57 +0300

    Add files via upload

---
+++ testCat.py
@@ -0,0 +1,7 @@
+from Cat import Cats
+
+pet_1 = Cats("Барон", "male", 2)
+pet_2 = Cats("Сэм", "male", 2)
+
+print(pet_1.get_name(), pet_1.get_sex(), pet_1.get_age())
+print(pet_2.get_name(), pet_2.get_sex(), pet_2.get_age())
